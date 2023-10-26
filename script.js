15f5d8b5971bce17d45ff96ea61d0d2134f94e98
Roman <igoshinr.estho@gmail.com>
2023-10-26 04:54:31 +0300

    com

---
+++ script.js
@@ -0,0 +1,97 @@
+/*Задание 1: "Управление персоналом компании"
+
+Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+Свойство name (имя) - строка, имя сотрудника.
+Метод displayInfo() - выводит информацию о сотруднике (имя) в консоль.
+
+Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).*/
+
+class Employee {
+    name = 'name';
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}`);
+    }
+}
+
+class Manager extends Employee {
+    department = 'department';
+
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}, Department: ${this.department}`);
+    }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
+
+/*Реализуйте класс Product (товар), который имеет следующие свойства и методы:
+Свойство name - название товара.
+Свойство price - цена товара.
+Свойство quantity - количество товара.
+
+Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+Свойство id (номер заказа) - число, уникальный номер заказа.
+Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
+Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.*/
+
+class Product {
+    name = 'name';
+    price = 0;
+    quantity = 0;
+
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+let id = 0;
+
+class Order {
+    id = 0;
+    products = [];
+
+    constructor() {
+        this.id = id++;
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    getTotalPrice() {
+        let totalPrice = 0;
+        this.products.forEach(product => {
+            totalPrice += product.price * product.quantity;
+        });
+        return `Price: ${totalPrice}`;
+    }
+}
+
+const order = new Order();
+
+const product1 = new Product("Phone", 500, 2);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100, 1);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice());
+
